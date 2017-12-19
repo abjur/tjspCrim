bdc06dacbb10d7c3b6a9bf0f9eb871c585a3b1b0
Milene Farhat <milene.farhat@gmail.com>
2017-12-19 15:02:28 -0200

    delete unnecessary file

--- R/run.R
+++
@@ -1,7 +0,0 @@
-
-# Download and parse cjsg
-d_cjsg <- cjsg_download() %>% 
-  mutate(id_lawsuit = stringr::str_replace_all(id_lawsuit, "[:punct:]",""))
-
-# Download and parse cposg
-d_cposg <- cposg_download()
