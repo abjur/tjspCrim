3950824a3fc40168b9e61eceee96171efb1139e6
Milene Farhat <milene.farhat@gmail.com>
2018-01-12 19:46:17 -0200

    call downloads

---
+++ R/call_download.R
@@ -0,0 +1,5 @@
+# Download and parse cjsg
+cjsg_download(path = '/home/storage/abj/projects/tjspCrim/d_cjsg')
+
+# Download and parse cposg
+cposg_download(path = '/home/storage/abj/projects/tjspCrim/d_cposg')
