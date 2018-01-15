7699112499fcce2e67c6f6846ae5b4b9b00018f8
Milene Farhat <milene.farhat@gmail.com>
2018-01-15 20:38:06 -0200

    add df d_cjsj to call cposg_download(path)

--- R/call_download.R
+++ R/call_download.R
@@ -1,5 +1,10 @@
 # Download and parse cjsg
 cjsg_download(path = '/home/storage/abj/projects/tjspCrim/d_cjsg')
 
+# criar d_cjsg para rodar cposg_download(path) . 
+##  A função utiliza a variável numérica d_cjsg$id para baixar os processos.
+d_cjsg <- readRDS("/home/storage/abj/projects/tjspCrim/data/d_cjsg.rds") %>%
+  pre_process_cjsg()
+
 # Download and parse cposg
 cposg_download(path = '/home/storage/abj/projects/tjspCrim/d_cposg')
